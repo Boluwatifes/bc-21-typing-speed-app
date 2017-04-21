8b3826a1053db79238ae039ccc93ff5f39c28568
Bamidele Daniel <greatbolutife@gmail.com>
2017-04-21 22:08:34 +0100

    Added code documentation

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var firebase = require('firebase');
 
 const app = express();
 
-// Initialize Firebase
+// Firebase Configuration
 const config = {
 	apiKey: "AIzaSyC6nIqvcahbeuGtWogrsRzmwXBk7Bjl5rQ",
 	authDomain: "test-project-3341a.firebaseapp.com",
@@ -14,45 +14,72 @@ const config = {
 	messagingSenderId: "881454952815"
 };
 
+// Initialize firebase
 firebase.initializeApp(config);
 
+// Set the database collection 
 const ref = firebase.database().ref();
 
+// Serve static files
 app.use(express.static('./assets'));
+
+// Use body-parser
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+
+// Set the view engine to Embedded Javascript
 app.set('view engine', 'ejs');
+
+// Set EXPRESS port
 app.listen(process.env.PORT || 3000);
 
-app.get('/', (req, res) => res.render('index'));
+// Listen and respond to GET request on the index route
+app.get('/', (req, res) => {
+	res.render('index');
+});
 
-app.get('/test', (req, res) => res.render('test'));
+// Listen and respond to GET request on the test route
+app.get('/test', (req, res) => {
+	res.render('test');
+});
 
+// Listen and respond to GET request on the leaderboard route
 app.get('/leaderboard', (req, res) => {
+	// set new collection
 	let childRef = ref.child('results');
+
+	// Get data from firebase database
 	childRef.once('value', (snap) => {
 		let dbData = snap.val();
 		let holdData = [];
+
+		// Loop through the data received from the database and save it to the `holdData` variable
 		for(const data in dbData){
 			const temp = {};
 			temp.name = dbData[data].name;
-			temp.wpm = dbData[data].wpm;
+			temp.wpm = parseInt(dbData[data].wpm);
 			temp.nwpm = dbData[data].nwpm;
 			temp.errors = dbData[data].errors;
 			temp.accuracy = dbData[data].accuracy;
 			holdData.push(temp);
 		}
-		holdData.sort((a,b) =>{return b.wpm > a.wpm});
-		console.log(holdData);
+
+		// Sort the data in decending order
+		holdData.sort((a,b) => {
+			return (b.wpm > a.wpm) ? 1 : ((a.wpm > b.wpm) ? -1 : 0);
+		});
+
+		// Renders the leaderboard.ejs file
 		res.render('leaderboard', { 
 		    results: holdData,
 		});
 	});
 });
 
+// Handle POST request from the test page and save data to the database
 app.post('/saveUser', (req, res) => {
-	let newData = req.body;
+  let newData = req.body;
 	let resRef = ref.child('results');
 	let newRes = resRef.push(newData);
 	res.send({ success: true});
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
+// Starts jQuery 
 $(function(){
+	// Listen to click event on `#submit` and prepare session for a new user
 	$('body').on('click', '#submit', (e) => {
 		e.preventDefault();
 		$('body #error').hide();
@@ -6,62 +8,118 @@ $(function(){
 		if(name.length < 3) {
 			$('body .content').prepend('<div class="alert alert-danger" id="error">Name must be longer than 2 characters</div>')
 		}else{
+			// Save user's name to the local sessionStorage
 			sessionStorage.name = name;
-			console.log(sessionStorage.name);
+
+			// Redirect user to test page
 			window.location.href = '/test';
 		}
 	});
 
+	// Assigns value to username variable
 	let username = sessionStorage.name || 'Guest';
 
+	// Prints the username to the page
 	$('#name-holder').prepend('<h3> Hi, <span class="green">' + username + ' </span></h3>');
 
 	const typingText = $('#typingText');
 
-	typingText.bind('copy cut paste', (e) => e.preventDefault());
+	// Prevents copy, cut and paste action in the user's textbox
+	typingText.bind('copy cut paste', e => e.preventDefault());
 
+	/** 
+	 * @function
+	 * @name calculateSpeed
+	 * This function contains the functions that calculates the gross word per minute and the net word per minute
+	 */
 	const calculateSpeed = {
-		calcWPM(words){
+
+		/**
+		 * Calculates the word per minute by dividing the total characters of the input string by 5
+		 * @param {String} words
+		 * @returns {Number}
+		 */
+		calcWPM(words) {
 			return Math.round(words.length / 5)
 		},
-		calcNET(words, typed){
+
+		/**
+		 * Calculates the net word per minute by subtracting the total errors from the words typed
+		 * @param {String} words
+		 * @param {String} typed
+		 * @returns {Object}
+		 */
+		calcNET(words, typed) {
 			let wordArr = words.split('');
 			let typedArr = typed.split('');
 			var errors = 0;
-			for(let i = 0; i < typedArr.length; i++){
+			for(let i = 0; i < typedArr.length; i++) {
 				if(typedArr[i] !== words[i]){
 					errors += 1;
 				}
 			}
+
+			// Calculate the Net Word Per Minute
 			const result = Math.round((typed.length / 5) - errors);
+
+			// Returns the result as json object
 			return JSON.parse(`{"result": ${result}, "errors": ${errors}}`);
-		}
+		},
 	};
 
+	// Listen to `focus` event on `#typingText' and fire
 	typingText.on('focus', () => {
+		
+		// Set default time to 60s
 		var num = 60;
 
+		/**
+		 * Process the user's input
+		 * Saves the data to database
+		 * Manipulates the DOM
+		 */
 		const processResults = () => {
+
+			// Captures the user's input
 			const textVal = typingText.val();
-			console.log(textVal);
+
+			// Original Text
 			const origText = "One day Jimmy Westfielder was walking home from school and saw a big, brown dog walking behind him. At first he was a bit startled at the fact that the dog was just calmly walking behind him. Jimmy stopped and turned around to face the dog. When Jimmy looked into the dog's eyes he became  Cognizant that the dog meant no harm at all. Jimmy bent down to the dog's level and put one of his hands out. As soon as Jimmy did so the dog began to lick  Jimmy's hand. Jimmy then checked to see if the dog had a collar, which he didn't, and when he didn't find a collar he then said, \"Hello my name is Jimmy.\"";
 			const copyText = $('#copyText');
 			const nameHolder = $('#name-holder p');
+
+			// Fades out the User's input box
 			typingText.fadeOut(1000, () => {
+
+				// Fades out the Original text
 				copyText.fadeOut(1000);
+
+				// Fades out the introductory p element
 				nameHolder.fadeOut(1000, () => {
+
+					// Replace the introductory text
 					nameHolder.html('Your time is up! Your typing speed is been analyzed by our Algorithm');
+					
+					// Display the introductory text with fade effect
 					nameHolder.fadeIn(1000, () => {
+
+						// Append a loading gif to the main content area
 						$('.loading').html('<img src="/images/cube.gif" class="img-responsive"/>').css({ 'display' : 'block' });
+						
+						// Check to see if the user's input is empty
 						if(textVal === ''){
 							const returnText = `<div class="results"><p>Hey, it seems you didn't type anything into the textbox. Please try again!</p></div>`;
 							$('.loading').hide();
 							$('#name-holder').after(returnText);
 						}else{
+
+							// Call the calculateSpeed function earlier declared to get results
 							const wpm = calculateSpeed.calcWPM(textVal);
 							const nwpm = calculateSpeed.calcNET(origText, textVal);
 							const accuracy = Math.floor(((textVal.length - nwpm.errors) / textVal.length) * 100);
 							const netwpm = nwpm.result > 0 ? nwpm.result : 0;
+
+							// Build results to display back to the user
 							let returnText = `<div class="results">`;
 							returnText += `<h2>Here is your result</h2>`;
 							returnText += `<div class="col-md-6 col-sm-8 col-xs-8 text-right"><span> Gross Word Per Minute: </span></div>`;
@@ -79,8 +137,9 @@ $(function(){
 								</div>`;
 							returnText += `<div class="clear"></div>`;
 							returnText += `</div>`;
-							const data =  JSON.parse(`{"name": "${sessionStorage.name}", "wpm": ${wpm}, "accuracy": ${accuracy}, "nwpm": ${netwpm}, "errors": ${nwpm.errors}}`);
-							console.log(data);
+
+							// Pass the user's results to the `saveUser` route for storage into the database
+							const data = JSON.parse(`{"name": "${sessionStorage.name}", "wpm": ${wpm}, "accuracy": ${accuracy}, "nwpm": ${netwpm}, "errors": ${nwpm.errors}}`);
 							$.ajax({
 								type: 'POST',
 								url: '/saveUser',
@@ -93,6 +152,8 @@ $(function(){
 									console.log(error);
 								}
 							});
+
+							// Display the results to the user
 							$('.loading').hide();
 							$('#name-holder').after(returnText);
 						}
@@ -101,20 +162,25 @@ $(function(){
 			});
 		};
 
+		// Start timer
 		var interval = setInterval(() => {
 			let timeCont = $('#timer span');
-			if(num !== 0){
+			if(num !== 0) {
 				num -= 1;
 				if(num > 20){
 					timeCont.html((num) + 's');
-				}else if(num < 20){
+				}else if(num < 20) {
 					timeCont.css({
 						'color' : 'red'
 					});
 					timeCont.html((num) + 's');
 				}
-			}else if(num === 0){
+			}else if(num === 0) {
+
+				// Stops the timer if num reaches 0
 				clearInterval(interval);
+
+				// Process the User's input
 				processResults();
 			}
 		}, 1000);
